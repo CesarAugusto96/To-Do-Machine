a9e0b95ef2117ced78306ede3c6660d6797c2811
Cesar Caballero <cesarcaballero203@gmail.com>
2023-12-04 22:17:40 -0600

    v2.0

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './platzi.webp';
-import './App.css';
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList';
+import { TodoItem } from './TodoItem';
+import { CreateTodoButton } from './CreateTodoButton';
+
+
+
+//ToDos que aparecen por default en el interfaz de la app.
+const defaultToDos = [
+  { text: "Ir al Super", completed: false},
+  { text: "Reunión de trabajo", completed: true},
+  { text: "Ir al nether", completed: true},
+  { text: "Recoger al michi de la estética", completed: false},
+];
+
+
+// Cuando el nombre de la función, empieza con letra
+//mayúscula, se trata de un componente de React.
+
+//La siguiente sintáxis corresponde a la extensión .jsx
+
+/*
+Cada fragmento de los componentes debe estar contenido dentro 
+de la etiqueta <React.Fragment></React.Fragment> (Es necesario
+importar: "import React from 'react';" ) o en su defecto, 
+usando solo <> </> 
+*/
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+//Los componentes de React, se deben renderizar
+//con los simbolos <></>
+<>
+
+{/*Añadiendo los componente de react al componente principal*/}
+
+    <TodoCounter completed={3} total={5} />
+    <TodoSearch/>
+
+    <TodoList>
+      {defaultToDos.map(todo => (
+        <TodoItem 
+        key={todo.text} 
+        text={todo.text}
+        completed={todo.completed} 
+        />
+      ))}
+    </TodoList>
+    
+    <CreateTodoButton/>
+
+</>
+
   );
 }
 
